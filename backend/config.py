7ea1232703c98e0c5fcdce920c0971c738f059e6
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2026-01-28 10:11:36 +0000

    updated database config for new local env

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:brainstorm@localhost:5432/vehicle_booking"
+        "postgresql://postgres:brainstorm@localhost:5432/vehicle_booking_db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
