154d817c8b0cfabe7b6cdb2e19f35bd24ef503c8
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2026-02-11 09:38:37 +0000

    added jwt

--- frontend/src/pages/login/ProtectedRoutes.jsx
+++ frontend/src/pages/login/ProtectedRoutes.jsx
@@ -1,20 +1,28 @@
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
 
 function ProtectedRoute({ children, allowedRoles }) {
-  const role = localStorage.getItem("role");
+  const token = localStorage.getItem("access_token");
 
-  if (!role) {
-    // Not logged in
+  if (!token) {
     return <Navigate to="/" replace />;
   }
 
-  if (allowedRoles && !allowedRoles.includes(role)) {
-    // Logged in but not allowed
+  try {
+    const decoded = jwtDecode(token);
+    const role = decoded.role;
+
+    if (allowedRoles && !allowedRoles.includes(role)) {
+      return <Navigate to="/" replace />;
+    }
+
+    return children;
+  } catch (error) {
+    // Invalid or expired token
+    localStorage.clear();
     return <Navigate to="/" replace />;
   }
-
-  // User is allowed
-  return children;
 }
 
 export default ProtectedRoute;
