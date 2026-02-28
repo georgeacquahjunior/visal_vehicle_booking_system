00faa71be66fabd13d8258da046e9935058a30be
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2026-02-28 12:41:47 +0000

    updated the db config with render

--- backend/config.py
+++ backend/config.py
@@ -2,11 +2,16 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", SECRET_KEY)
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_HOURS", "8")))
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql://postgres:brainstorm@localhost:5432/vehicle_booking_db"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+        hours=float(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_HOURS", "0.5"))
     )
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {"sslmode": "require"}
+    }
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
