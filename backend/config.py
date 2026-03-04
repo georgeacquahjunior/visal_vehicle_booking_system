0469711b9c14dafa640ebcbc023e51bd67f7c3c9
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2026-03-04 15:52:43 +0000

    updated token hrs

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(
-        hours=float(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_HOURS", "0.1"))
+        hours=float(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_HOURS", "4.0"))
     )
 
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
